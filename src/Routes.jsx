276670561a6c3f87e5f27b0f795dfe0f38149da8
Fahadayyoub <30391844+Fahadayyoub@users.noreply.github.com>
2024-03-15 09:50:20 +0500

    All done
    All done

---
+++ src/Routes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import HomepageFour from "pages/HomepageFour";
+
+const ProjectRoutes = () => {
+  let element = useRoutes([{ path: "/", element: <HomepageFour /> }]);
+
+  return element;
+};
+
+export default ProjectRoutes;
